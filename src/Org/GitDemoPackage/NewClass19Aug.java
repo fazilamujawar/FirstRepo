bba3864216aa893ff5b01781c6045aeb0f40a2a6
mujawarfazila <108861970+mujawarfazila@users.noreply.github.com>
2022-08-19 12:10:44 +0530

    For Pull request

--- src/Org/GitDemoPackage/NewClass19Aug.java
+++ src/Org/GitDemoPackage/NewClass19Aug.java
@@ -5,6 +5,7 @@ public class NewClass19Aug {
 	public static void main(String[] args) {
 
 		System.out.println("To check the updates");
+		System.out.println("Changes made for pull request");
 
 	}
 
